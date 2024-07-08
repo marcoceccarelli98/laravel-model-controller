c234d7b22ea23895f5885424c532dc2d0385d1a6
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-08 14:59:53 +0200

    [UPD] Created base from template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
+    <title>Movies</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
     <h1>
-        Prova
+        Movies
     </h1>
-
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
 
 </body>
 
